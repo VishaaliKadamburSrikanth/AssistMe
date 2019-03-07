51aa17bc355748d45e6ddf2081854d787b21ac3c
Archanaapriya Nallasivan <archanaa.priya.n@gmail.com>
2019-03-06 21:29:38 -0400

    added accomodation code

--- app/src/main/java/com/example/menu/AccomodationFragment.java
+++ app/src/main/java/com/example/menu/AccomodationFragment.java
@@ -1,5 +1,7 @@
 package com.example.menu;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -17,4 +19,13 @@ public class AccomodationFragment extends Fragment {
     {
         return inflater.inflate(R.layout.fragment_accomodation, container, false);
     }
+    public void open(View view)
+    {
+        Intent browserIntent =new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.kijiji.ca/\n"));
+        startActivity(browserIntent);
+
+        Intent browserIntent1 =new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.airbnb.ca/\n"));
+        startActivity(browserIntent1);
+    }
+
 }
